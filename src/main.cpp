39bd9b23556f696a57a19096294a44e93460c884
ArshAnan <arshanand2524@gmail.com>
2026-03-03 17:07:37 -0500

    created a listening port

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <sys/socket.h>
+
+// Making sure the port is listening to the client
+void listenToClient(int port) {
+    // Create a socket
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0) {
+        std::cerr << "Failed to create a socket" << "\n";
+        exit(1);
+    }
+}
+
+void setupSocket(int server_fd, int port) {
+    // Bind the socket to the port
+
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    listenToClient(6379);
     return 0;
 }
